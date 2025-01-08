b54c9011bf0edaceb5c38c075c9e9be43312c797
srskulkarni <srushti.sskulkarni@gmail.com>
2025-01-07 17:24:12 -0800

    header image issue

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './Header.css'
+import {assets} from '../../assets/assets'
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className="header_img" style={{
+      height: '45vw',
+      margin: '40px auto',
+      background: `url(${headerImg}) no-repeat center center`,
+      backgroundSize: 'cover',
+      borderRadius: '50px',
+    }}>
+      <div className='header'>
       <div className="header-contents">
         <h2>Order your favourite food here! </h2>
         <p> Choose from a diverse menu featuring delectable array of dishes crafted with the finest ingredients and culinary expertise.</p>
         <button> View Menu </button>
       </div>
+    </div>
     </div>
   )
 }
