854510dc7af33ca7cba141f2b1418bbfe6d2acfb
srskulkarni <srushti.sskulkarni@gmail.com>
2025-01-17 17:14:58 -0800

    Update orderController.js

--- Backend/controller/orderController.js
+++ Backend/controller/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder =  async(req,res) =>{
 
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://foodapp-frontend-fltb.onrender.com"
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
