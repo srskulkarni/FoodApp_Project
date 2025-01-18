acdf442aeb899c010cc8f73fdaed1317ed38cadc
srskulkarni <srushti.sskulkarni@gmail.com>
2025-01-17 17:01:50 -0800

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "http://localhost:4000"
+  //const url = "http://localhost:4000"
+  const url = "https://foodapp-project.onrender.com"
   return (
     <div>
       <ToastContainer/>
