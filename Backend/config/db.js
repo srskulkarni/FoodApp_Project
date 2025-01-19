2dacc2c35f01ee7b7d29976b3c72b3034aba1ea2
srskulkarni <srushti.sskulkarni@gmail.com>
2025-01-18 19:18:28 -0800

    db.js edited

---
+++ Backend/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+require('dotenv').config();
+const mongoKey = process.env.MONGO_DB_KEY;
+
+export const connectDB = async() => {
+    await mongoose.connect(`mongodb+srv://${mongoKey}@cluster0.k8x41.mongodb.net/FoodApp`).then(()=> console.log('DB Connected'));
+}
