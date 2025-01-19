357572944b9a95999886b673d8e0e984f3bcf334
srskulkarni <srushti.sskulkarni@gmail.com>
2025-01-18 18:55:36 -0800

    .gitignore changed

--- Backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://srskulkarni:0tLpg6DiT4e7jLg@cluster0.k8x41.mongodb.net/FoodApp').then(()=> console.log('DB Connected'));
-}
